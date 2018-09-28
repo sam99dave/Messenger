6483c036c505d0b82ea4e204bc5758363ab8a13d
Samuel <sam99dave@gmail.com>
2018-09-28 19:47:39 +0530

    Flexible File Handling

--- Messenger/src/Test2.java
+++ Messenger/src/Test2.java
@@ -1,15 +1,17 @@
 import java.io.*;
 import java.util.*;
 
-public class Test2 /*implements Runnable*/{
-    String group_name;
-    Test2(String group_name){
-        this.group_name = group_name;
+public class Test2 implements Runnable{
+    Group group;
+    Member current_user;
+    Test2(Group group, Member current_user){
+        this.group = group;
+        this.current_user = current_user;
     }
-    public static void main(String [] args) /* public void run if using Runnable*/{
+    /*public static void main(String [] args)*/  public void run {
 
         // The name of the file to open.
-        String fileName = group_name + ".txt";
+        String fileName = group.Name + ".txt";
 
         // This will reference one line at a time
         String line = null;
@@ -21,8 +23,16 @@ public class Test2 /*implements Runnable*/{
             // Always wrap FileReader in BufferedReader.
             BufferedReader bufferedReader =
                     new BufferedReader(fileReader);
-
+            // The first while reads the data at launch
             while((line = bufferedReader.readLine()) != null) {
+                if(line.equals(group.Owner.Name + " remove " + current_user.Name)){
+                    break; // current_user is removed
+                }
+                System.out.println(line);
+            }
+            // This while reads the messages after initial reading of data
+            // So that the current_user doesnt encounter his messages twice
+            while(!((line = bufferedReader.readLine()).equals(current_user.Name + " has been removed") || (line = bufferedReader.readLine()).equals(current_user.Name + " left the lobby"))){
                 System.out.println(line);
             }
 
--- Messenger/src/Test3.java
+++ Messenger/src/Test3.java
@@ -4,23 +4,29 @@
 
 import java.io.*;
 import java.util.*;
-public class Test3 /* implements Runnable*/{
-    String group_name;
-    Test3(Sring group_name, /* Message if to be stored only */){
-        this.group_name = group_name;
+
+public class Test3 implements Runnable{
+    Group group;
+    FileWriter fileWriter;
+    Member current_user;
+    Test3(Group group,FileWriter fileWriter, Member cuurent_user){
+        this.group = group;
+        this.filewriter = filewriter;
+        this.current_user = cuurent_user;
     }
-    public static void main(String [] args) /* public void run if using Runnable*/{
+    /*public static void main(String [] args)*/  public void run {
         Scanner sc = new Scanner(System.in);
+        int flag = 0;
         // The name of the file to open.
-        String fileName = group_name + ".txt";
+        String fileName = group.Name + ".txt";
 
         try {
             // Assume default encoding.
-            if(// groupList doesnt contain group_name)  // This will create new file named group_name.txt
+            /*if()  // This will create new file named group_name.txt
             {
             FileWriter fileWriter =
                     new FileWriter(fileName);
-            }
+            }*/
             // Always wrap FileWriter in BufferedWriter.
             BufferedWriter bufferedWriter =
                     new BufferedWriter(fileWriter);
@@ -30,15 +36,25 @@ public class Test3 /* implements Runnable*/{
             String s = "" ;
             StringBuilder input = new StringBuilder(s);
 
-            while(input.toString() != "Stop"){
+            while(input.toString() != "Stop"){s
                 input = input.replace(0, input.length(), sc.nextLine());
                 if(input.toString().equals("Stop")){
                     break;
                 }
-                bufferedWriter.write(input.toString());
-                bufferedWriter.newLine();
+                else if(input.toString().equals(group.Ownwer.Name + " " + "remove " + current_user.Name)){
+                    bufferedWriter.write(current_user.Name + " has been removed");
+                    bufferedWriter.newLine();
+                    flag = 1;
+                    break;
+                }
+                else {
+                    bufferedWriter.write(input.toString());
+                    bufferedWriter.newLine();
+                }
             }
-
+            if(flag == 1) {
+                bufferedWriter.write(current_user.Name + " left the lobby");
+            }
             // Always close files.
             bufferedWriter.close();
         }
